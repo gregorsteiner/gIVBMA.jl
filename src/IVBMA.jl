5d80046bde70c62d596b24608c7dc207a8cded65
Gregor Steiner <gregorsteiner7@gmail.com>
2024-10-17 15:53:48 +0100

    Implemented simple IV

--- src/IVBMA.jl
+++ src/IVBMA.jl
@@ -4,13 +4,14 @@ using LinearAlgebra, Distributions, Statistics
 using InvertedIndices, SpecialFunctions
 using StatsPlots
 
-export ivbma, ivbma_2c, lpd, posterior_predictive, plot
+export ivbma, ivbma_2c, iv_fit, lpd, posterior_predictive, plot
 
 
 include("ivbma_priors.jl")
 include("ivbma_fit.jl")
 include("ivbma_2c.jl")
 include("ivbma_tools.jl")
+include("iv_fit.jl")
 
 
 end
